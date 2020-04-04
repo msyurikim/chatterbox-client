17e26ad97ccd92ee885bce07f02df62a1fae5367
stephenJohnGiardina <stephengia@yahoo.com>
2020-04-04 15:05:37 -0400

    Successfully protected against XSS attacks

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -10,6 +10,9 @@ var MessagesView = {
   //originally render: function
   renderMessage: function(message) {
     if (message.text && message.username && message.roomname) {
+      message.text = Messages.convertHTML(message.text);
+      message.username = Messages.convertHTML(message.username);
+      message.roomname = Messages.convertHTML(message.roomname);
       let newMessage = MessageView.render(message);
       $('#chats').append(newMessage);
     }
